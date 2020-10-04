18c27f816106a46b986566846a466fe8e400f9bc
Fefeprogrammer <50031002+Fefeprogrammer@users.noreply.github.com>
2020-10-04 04:34:40 -0300

    Primeiro Commit

--- Shared/Program.cs
+++ Shared/Program.cs
@@ -4,18 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sockets_Csharp
+namespace Shared
 {
     class Program
     {
         static void Main(string[] args)
         {
-            echoServer server = new echoServer();
-
-            server.Start();
-
-            Console.WriteLine("Server running");
-            Console.ReadLine();
         }
     }
 }
